3a3f1c268403559b2b103272a7612b45231a014f
riwahoussari <houssaririwa63@gmail.com>
2025-03-04 22:15:37 +0200

    remove base from vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
-  base: '/ARTHYL-WEBSITE/'
+  plugins: [react(), tailwindcss(),]
 })
